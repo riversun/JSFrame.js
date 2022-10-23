bc28f250d4980291f7a5d6f89c92275e60f39d13
riversun <riversun.org@gmail.com>
2022-10-23 13:30:38 +0900

    The following updates have been made to address the vulnerability
    - webpack version from 4.x to 5.x
    - webpack-devserver version from 3.x to 4.x

--- webpack.config.js
+++ webpack.config.js
@@ -6,58 +6,66 @@ const webpack = require('webpack');
 
 module.exports = (env, argv) => {
 
-  const conf = {
-    mode: 'development',//production,development
-    devServer: {
-      open: true,
-      openPage: "index.html",
-      contentBase: path.join(__dirname, '/public'),
-      watchContentBase: true,
-      disableHostCheck: true,
-      port: 8080,
-    },
-    entry: {
-      jsframe: './src/index.js',
-    },
-    output: {
-      path: path.join(__dirname, "lib"),
-      filename: argv.mode === 'production' ? `[name].min.js` : `[name].js`,
-      libraryTarget: 'umd'
-    },
-    optimization: {
-      minimizer: [new TerserPlugin({
-        //extractComments: true,
-        //cache: true,
-        //parallel: true,
-        //sourceMap: true,
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
-        }
-
-      })],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            { loader: "style-loader" },
-            { loader: "css-loader" }
-          ]
+    const conf = {
+        mode: 'development',//production,development
+        devServer: {
+            port: 8080,
+            open: {
+                target: ["index.html"],
+            },
+            allowedHosts: "all",
+            static:
+                [
+                    {
+                        directory: path.join(__dirname, `public`),
+                        publicPath: "/",
+                        serveIndex: false,
+                        watch: true,
+                    },
+                ],
         },
-      ],
-    },
-    plugins: [
-      new webpack.BannerPlugin(`[name] v${version} Copyright (c) 2007-2020 Tom Misawa`)
-    ]
-  };
+        entry: {
+            jsframe: './src/index.js',
+        },
+        output: {
+            path: path.join(__dirname, "lib"),
+            filename: argv.mode === 'production' ? `[name].min.js` : `[name].js`,
+            libraryTarget: 'umd'
+        },
+        optimization: {
+            minimizer: [new TerserPlugin({
+                //extractComments: true,
+                //cache: true,
+                //parallel: true,
+                //sourceMap: true,
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                    },
+                }
 
-  if (argv.mode !== 'production') {
-    conf.devtool = 'inline-source-map';
-  }
+            })],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        {loader: "style-loader"},
+                        {loader: "css-loader"}
+                    ]
+                },
+            ],
+        },
+        plugins: [
+            new webpack.BannerPlugin(`[name] v${version} Copyright (c) 2007-2020 Tom Misawa`)
+        ]
+    };
 
-  return conf;
+    if (argv.mode !== 'production') {
+        conf.devtool = 'inline-source-map';
+    }
+
+    return conf;
 };
 
